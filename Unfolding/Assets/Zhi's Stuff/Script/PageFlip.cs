d1d825b974f91394f8a54691c5c5941b21fadcdf
SleepyZhi <laidezhi317@gmail.com>
2024-02-05 23:46:32 +0800

    Finished page flipping
    Page flipping is working now it seems. Now the other stuff. Probs not the best implementation but I will fix when needed.
    *No mouse implementation, lol

--- Unfolding/Assets/Zhi's Stuff/Script/PageFlip.cs
+++ Unfolding/Assets/Zhi's Stuff/Script/PageFlip.cs
@@ -1,15 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PageFlip : MonoBehaviour
 {
+    [Tooltip("The higher, the more you need to swipe")]
+    public float flipSensitivity = 5000f;
+
     private Ray ray;
     private RaycastHit hit;
 
     private Vector2 startPos, pos;
 
+    private Touch touch;
+    private Animator anim;
+
     private bool clicked;
+
+    private float frame; 
+
+    private void Start()
+    {
+        frame = 0;
+        anim = GetComponentInParent<Animator>();
+        anim.speed = 0;
+    }
 
     void Update()
     {
@@ -32,31 +48,46 @@ public class PageFlip : MonoBehaviour
             clicked = false;
         }
 
-        Touch touch;
-        Animator anim = GetComponentInParent<Animator>();
-        float frame = 0;
-
         if (clicked)
         {
             touch = Input.GetTouch(0);
-            anim.speed = 0;
-            Debug.Log("It works, plus" + anim.name);
 
-            if (Input.touches[0].phase == TouchPhase.Moved)
+            if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Moved)
             {
+                startPos = touch.position - touch.deltaPosition;
                 pos = touch.position - startPos;
 
-                frame += (-pos.x) / 360;
+                frame += (-pos.x) / flipSensitivity;
                 Debug.Log(frame);
                 anim.Play("IFlip", 0, frame);
             }
         }
     }
-// BUGS I FOUND
-/* - When flipping the other way, it will instantly snap to the end, probably due to how it is coded in positional based, rather than like an additive type of way. I'm not sure why it won't let me but
- * I'll just have make do with this for now.
- *  - Loading and deloading assets in. I have to find a way to instance stuff so that that pages can be renewed on the other side
- *   - Yes, I know, the mouse doesn't work, bohoo.
- */
 
+    private void FixedUpdate()
+    {
+        if (Input.touchCount == 0)
+        {
+            if (frame > 0.5)
+            {
+                frame += 0.01f;
+                anim.Play("IFlip", 0, frame);
+            }
+            else if (frame < 0.5)
+            {
+                frame -= 0.01f;
+                anim.Play("IFlip", 0, frame);
+            }
+
+            // My favourite line in programming
+            if (frame < 0)
+                frame = 0;
+            else if (frame > 1)
+                frame = 1;
+        }
+        // BUGS I FOUND
+        /* - Loading and deloading assets in. I have to find a way to instance stuff so that that pages can be renewed on the other side
+         * - Yes, I know, the mouse doesn't work, bohoo.
+         */
+    }
 }
