87df0df6060cb1d763a9628b05056e218edbf3c3
nyak2 <kaynchan123@gmail.com>
2024-02-05 14:37:49 +0800

    Player Controls and stage creation
    Added movement visualizer and change from double tap to single tap
    
    Test out how to create a stage with the model itself and testing navmesh stuff

--- Unfolding/Assets/Aik Chun'Stuff/scripts/NavMeshGenerator.cs
+++ Unfolding/Assets/Aik Chun'Stuff/scripts/NavMeshGenerator.cs
@@ -2,19 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshGenerator : MonoBehaviour
 {
     [SerializeField] private NavMeshSurface nav;
+    [SerializeField] private NavMeshData data;
+    [SerializeField] private NavMeshModifierVolume v;
     // Start is called before the first frame update
     void Start()
     {
-        nav.BuildNavMesh();
+        //UpdateMesh();
+        InvokeRepeating("UpdateMesh", 2,3);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateMesh()
+    {
+        //if(v.area == 1)
+        //{
+        //    v.area = 0;
+        //}
+        //else
+        //{
+        //    v.area = 1;
+        //}
+        //nav.BuildNavMesh();
+        nav.UpdateNavMesh(data);
     }
 }
--- Unfolding/Assets/Aik Chun'Stuff/scripts/PlayerMovement.cs
+++ Unfolding/Assets/Aik Chun'Stuff/scripts/PlayerMovement.cs
@@ -1,9 +1,6 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
-
 public class PlayerMovement : MonoBehaviour
 {
     public bool isRotate
@@ -12,20 +9,26 @@ public class PlayerMovement : MonoBehaviour
         set;
     }
     private RaycastHit hit;
-    private Vector2 startTouchPos;
-    private Vector2 EndTouchPos;
+    private bool tapToMove;
+
+    private List<Vector3> points = new List<Vector3>();
 
     [SerializeField] private NavMeshAgent player;
+    [SerializeField] private GameObject targetMark;
+    [SerializeField] private float rayDistance;
+    [SerializeField] private LayerMask groundLayer;
     void Start()
     // Start is called before the first frame update
     {
+        //lr = GetComponent<LineRenderer>();
         isRotate = false;
+        tapToMove = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isRotate)
+        if (isRotate || GameEventManager.isTouchObject)
         {
             //if(player.hasPath)
             //{
@@ -34,21 +37,118 @@ public class PlayerMovement : MonoBehaviour
             //}
             return;
         }
-        for (var i = 0; i < Input.touchCount; ++i)
+
+        if(Input.touchCount > 0)
         {
-            if (Input.GetTouch(i).phase == TouchPhase.Began && !GameEventManager.isTouchObject)
+            if (Input.GetTouch(0).phase == TouchPhase.Began /*&& !GameEventManager.isTouchObject*/)
             {
-                if (Input.GetTouch(i).tapCount == 2)
+                tapToMove = true;
+            }
+
+            if(Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                tapToMove = false;
+            }
+
+            if (Input.GetTouch(0).phase == TouchPhase.Ended)
+            {
+                if(tapToMove)
                 {
                     Ray touchRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
 
-                    if (Physics.Raycast(touchRay, out hit))
+                    if (Physics.Raycast(touchRay, out hit, rayDistance, groundLayer))
                     {
-                        player.SetDestination(hit.point);
+                        if(!hit.collider.CompareTag("Obstacles"))
+                        {
+                            player.SetDestination(hit.point);
+
+                        }
+                        //StartCoroutine(visualizeMovement());
+
                     }
                 }
             }
         }
 
+        if(player.velocity.magnitude > 0)
+        {
+            targetMark.transform.position = player.pathEndPosition;
+            targetMark.gameObject.SetActive(true);
+            //lr.positionCount = player.path.corners.Length;
+            //lr.SetPositions(player.path.corners);
+        }
+        else
+        {
+            targetMark.gameObject.SetActive(false);
+        }
+
+        //}
+
+        //for (var i = 0; i < Input.touchCount; ++i)
+        //{
+        //    if (Input.GetTouch(i).phase == TouchPhase.Began && !GameEventManager.isTouchObject)
+        //    {
+        //        if (Input.GetTouch(i).tapCount == 2)
+        //        {
+        //            Ray touchRay = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+
+        //            if (Physics.Raycast(touchRay, out hit))
+        //            {
+        //                player.SetDestination(hit.point);
+        //            }
+        //        }
+        //    }
+        //}
+
     }
+
+    //private IEnumerator visualizeMovement()
+    //{
+    //    yield return new WaitForSeconds(0.1f);
+    //    int i = 1;
+    //    //lr.positionCount = amtOfCorners;
+
+    //    int amtOfCorners = player.path.corners.Length;
+    //    points = player.path.corners.ToList();
+    //    while (i < amtOfCorners)
+    //    {
+    //        for (int j = 0; j < points.Count; j++)
+    //        {
+    //            lr.SetPosition(j, points[j]);
+    //        }
+    //        i++;
+    //    }
+    //}
+    //void checkForSlopes()
+    //{
+    //    if (points[0].y < points[1].y) // going up
+    //    {
+    //        Vector3 distance = points[1] - points[0];
+           
+    //        //float length = distance.magnitude;
+
+
+    //        if(startpos.y < transform.position.y)
+    //        {
+    //            startpos = new Vector3(transform.position.x,transform.position.y / 2,transform.position.z);
+    //        }
+
+    //        //for(int i = 0; i < 2; i++)
+    //        //{
+    //            NavMesh.(1, startpos);
+    //        //}
+
+    //        lr.SetPositions(points.ToArray());
+
+    //    }
+    //    else if (points[0].y > points[1].y) // going down
+    //    {
+
+    //    }
+    //    else
+    //    {
+    //        lr.SetPositions(points.ToArray());
+    //    }
+
+    //}
 }
