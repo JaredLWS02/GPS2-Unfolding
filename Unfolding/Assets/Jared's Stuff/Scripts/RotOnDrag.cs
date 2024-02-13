b40b9d829f8fe91bb801a8c48e086ad43a8907db
JaredLWS <0135860@kdu-online.com>
2024-02-14 02:36:04 +0800

    diff rotatation style

--- Unfolding/Assets/Jared's Stuff/Scripts/RotOnDrag.cs
+++ Unfolding/Assets/Jared's Stuff/Scripts/RotOnDrag.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class rotOnDrag : MonoBehaviour
 {
-    float rot;
+    float rotx,roty,rotz;
     float precision = 0.9999f;
     bool rotAble = true;
     Quaternion targetAngle = Quaternion.Euler(-91, 0, 0);
 
     void Update()
     {
-        transform.Rotate(rot, 0, 0);
+        transform.Rotate(rotx, 0, 0);
         if(rotAble == false)
         {
             if (Mathf.Abs(Quaternion.Dot(this.transform.rotation, targetAngle)) > precision)
             {
-                rot = 0;
+                rotx = 0;
+                roty = 0;
+                rotz = 0;
             }
         }
     }
@@ -25,25 +27,26 @@ public class rotOnDrag : MonoBehaviour
     {
         if(rotAble == true)
         {
-            rot = -0.5f;
+            rotx = -0.5f;
             rotAble = false;
         }
     }
 
+    public void rotatoY()
+    {
+        if (rotAble == true)
+        {
+            roty = -0.5f;
+            rotAble = false;
+        }
+    }
 
-    //Quaternion targetAngle2 = Quaternion.Euler(-1, 0, 0);
-
-    //else if (rotAble == true)
-    //{
-    //    if (Mathf.Abs(Quaternion.Dot(this.transform.rotation, targetAngle2)) > precision)
-    //    {
-    //        rot = 0;
-    //    }
-    //}
-
-    //else if (rotAble == false)
-    //{
-    //    rot = 0.5f;
-    //    rotAble = true;
-    //}
+    public void rotatoZ()
+    {
+        if (rotAble == true)
+        {
+            rotz = -0.5f;
+            rotAble = false;
+        }
+    }
 }
