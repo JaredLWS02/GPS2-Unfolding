d64f8433d57f7618bdbd0dd245a476a7a1ebfa49
Shan <eishana1709@gmail.com>
2024-02-17 19:05:10 +0800

    Changes to dialogue script

--- Unfolding/Assets/Shan/Shan scripts/DialogueTrigger.cs
+++ Unfolding/Assets/Shan/Shan scripts/DialogueTrigger.cs
@@ -1,18 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class DialogueTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private List<dialogueString> dialogueStrings = new List<dialogueString>();
+    [SerializeField] private Transform NPCTransform;
 
-    // Update is called once per frame
-    void Update()
+    private bool hasSpoken = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player") && !hasSpoken)
+        {
+            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, NPCTransform);
+            hasSpoken = true;
+        }
     }
+}
+
+[System.Serializable]
+
+public class dialogueString
+{
+    public string tex; //text that the Npc says
+    public bool isEnd; //if the line is the final line for the convo
+
+    [Header("Branch")]
+    public bool isQuesion;
+    public string answerOption1;
+    public string answerOption2;
+    public int option1IndexJump;
+    public int option2IndexJump;
+
+    [Header("Triggered Events")]
+    public UnityEvent startDialogueEvent;
+    public UnityEvent endDialogueEvent;
+
+
+
 }
