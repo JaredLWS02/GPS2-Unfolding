90a487fcdbca3350c24b25e3d71a45285cdd96a0
JaredLWS <0135860@kdu-online.com>
2024-02-17 21:21:35 +0800

    pain

--- Unfolding/Assets/Shan/Shan scripts/DialogueTrigger.cs
+++ Unfolding/Assets/Shan/Shan scripts/DialogueTrigger.cs
@@ -1,43 +1,43 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Events;
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using UnityEngine.Events;
 
-public class DialogueTrigger : MonoBehaviour
-{
-    [SerializeField] private List<dialogueString> dialogueStrings = new List<dialogueString>();
-    [SerializeField] private Transform NPCTransform;
+//public class DialogueTrigger : MonoBehaviour
+//{
+//    [SerializeField] private List<dialogueString> dialogueStrings = new List<dialogueString>();
+//    [SerializeField] private Transform NPCTransform;
 
-    private bool hasSpoken = false;
+//    private bool hasSpoken = false;
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player") && !hasSpoken)
-        {
-            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, NPCTransform);
-            hasSpoken = true;
-        }
-    }
-}
+//    private void OnTriggerEnter(Collider other)
+//    {
+//        if (other.CompareTag("Player") && !hasSpoken)
+//        {
+//            other.gameObject.GetComponent<DialogueManager>().DialogueStart(dialogueStrings, NPCTransform);
+//            hasSpoken = true;
+//        }
+//    }
+//}
 
-[System.Serializable]
+//[System.Serializable]
 
-public class dialogueString
-{
-    public string tex; //text that the Npc says
-    public bool isEnd; //if the line is the final line for the convo
+//public class dialogueString
+//{
+//    public string tex; //text that the Npc says
+//    public bool isEnd; //if the line is the final line for the convo
 
-    [Header("Branch")]
-    public bool isQuesion;
-    public string answerOption1;
-    public string answerOption2;
-    public int option1IndexJump;
-    public int option2IndexJump;
+//    [Header("Branch")]
+//    public bool isQuesion;
+//    public string answerOption1;
+//    public string answerOption2;
+//    public int option1IndexJump;
+//    public int option2IndexJump;
 
-    [Header("Triggered Events")]
-    public UnityEvent startDialogueEvent;
-    public UnityEvent endDialogueEvent;
+//    [Header("Triggered Events")]
+//    public UnityEvent startDialogueEvent;
+//    public UnityEvent endDialogueEvent;
 
 
 
-}
+//}
