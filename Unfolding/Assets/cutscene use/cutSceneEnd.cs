6d49e50482485b009f1dc6683b90b48a7e648ea1
nyak2 <kaynchan123@gmail.com>
2024-03-20 00:01:51 +0800

    kill me

--- Unfolding/Assets/cutscene use/cutSceneEnd.cs
+++ Unfolding/Assets/cutscene use/cutSceneEnd.cs
@@ -10,6 +10,7 @@ public class cutSceneEnd : MonoBehaviour
     [SerializeField] private Animator anim1;
     [SerializeField] private Animator anim2;
     [SerializeField] private Animator anim3;
+    [SerializeField] private UiTween ui;
 
     // Start is called before the first frame update
     void Start()
@@ -25,12 +26,21 @@ public class cutSceneEnd : MonoBehaviour
 
     public void endScene()
     {
+        StartCoroutine(transition());
+    }
+
+    private IEnumerator transition()
+    {
+        cutSceneCam.SetActive(false);
+        ui.BlackenScreenTransition();
+        yield return new WaitForSecondsRealtime(0.5f);
+        ui.UnBlackenScreenTransition(0.3f);
+        mainCam.SetActive(true);
         anim1.enabled = false;
         anim2.enabled = false;
         anim3.enabled = false;
-        cutSceneCam.SetActive(false);
-        mainCam.SetActive(true);
         cutScene.SetActive(false);
+
 
     }
 }
